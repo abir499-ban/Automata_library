d53aa040e4a508a7ebc9042de3e4e0c82a899bf5
Abir Banerjee <abirbanerjee793@gmail.com>
2025-03-12 12:42:41 +0530

    trim func added

--- automata_library/dfa.py
+++ automata_library/dfa.py
@@ -1,10 +1,20 @@
 
 class DFA:
     def __init__(self , states : list[str], alphabets : list[str] , initial_state : str, final_state : list[str]):
+        if initial_state not in states:
+            print("NOT a valid initial state")
+            return
+
+        if not set(final_state).issubset(set(states)):
+            print("Final States are not valid")
+            return
+        
+        self.__final_state  = final_state
         self.__states = states
         self.__alphabets  = alphabets
         self.__initial_state = initial_state
-        self.__final_state  = final_state
+        
+        
         self.__transitions = {}
         self.__GetTransitions()
 
@@ -14,11 +24,44 @@ class DFA:
             transition = {}
             for character in self.__alphabets:
                 print(f"Enter transition for state {state} on reading character {character}")
-                transition[character] = input()
+                next_state = input()
+
+                while True:
+                    if not next_state:
+                        print("Next State cannot be NULL")
+                    elif next_state not in self.__states:
+                        print("Next State is not Valid")
+                    else:
+                        break
+                transition[character] = next_state 
+
             self.__transitions[state] = transition
         
+    
+    def __Isreachable(self, state):
+        if state == self.__initial_state:
+            return True
         
+        reachable = False
+        for source in self.__states:
+            if reachable:
+                break
+            if state != source:
+                transitions = self.__transitions[source].values()
+                if state in transitions:
+                    reachable = True
+        return reachable
+
 
     
-    def sayHI(self):
-        print("HI")
+
+    def trim(self):
+        for state in self.__states:
+            if not self.__Isreachable(state):
+                del self.__transitions[state]
+    
+    def seeTransitions(self):
+        print(self.__transitions)
+
+    
+    
--- main/main.py
+++ main/main.py
@@ -1,12 +1,19 @@
 """this is the script Directory"""
 
+
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),  "..")))
 
 
 from automata_library import DFA
 
+def run_DFA():
+    my_DFA = DFA(["q0", "q1", "q2"], ["a", "b"], "q0", ["q1"])
+    my_DFA.trim()
+    my_DFA.seeTransitions()
 
-my_DFA = DFA(["q0", "q1", "q2"], ["0", "1"], "q0", ["q2"])
+
+if __name__=="__main__":
+    run_DFA()
